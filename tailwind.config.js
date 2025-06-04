3b120846107dee9cae05f43b890877b0c66ff37b
keshavagr025 <keshavagr204@gmail.com>
2025-06-04 14:29:27 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
- theme: {
+ theme: { 
   extend: {
     animation: {
       "spin-slow-y": "spinY 10s linear infinite",
